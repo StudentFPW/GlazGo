cfefe1a01b49f6f0c25fa1a12007cf5531ab50b1
LEONID JOFE <jofeleonids00@gmail.com>
2023-11-29 21:39:12 +0200

    Delete glazgo directory

--- glazgo/ats/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AtsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "ats"
-
-    def ready(self):
-        from . import signals
--- glazgo/chat/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-# Этот фрагмент кода определяет шаблон URL-адреса для соединений WebSocket в приложении Django.
-websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
-]
